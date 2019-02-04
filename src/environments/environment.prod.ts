95d61e361ec52be39a067901319af764a3b94c8c
frosty <aaronfrost@gmail.com>
2019-02-04 01:05:08 -0700

    add prod settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+    production: true,
+    MARVEL_API: {
+        PUBLIC_KEY: 'cc3db0c3e34cb42cd232aa6870257a8c',
+        PRIVATE_KEY: 'ab77fae750858f1688fc676522595c6b412cc960',
+    },
 };
